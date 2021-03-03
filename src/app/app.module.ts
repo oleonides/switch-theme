a450e38f24ec4f6e86f986d738a9134ad7f99e21
Olvert Leonides <olvert.leonides@gmail.com>
2021-03-03 09:53:38 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {ToolbarModule} from 'primeng/toolbar';
+import {ButtonModule} from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { MenuModule } from 'primeng/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    InputTextModule,
+    ToolbarModule,
+    ButtonModule,
+    RippleModule,
+    MenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/options.ts
@@ -0,0 +1,31 @@
+export const options = [
+  {
+    backgroundColor: '#fff',
+    buttonColor: '#ffc107',
+    headingColor: '#673ab7',
+    label: 'Saga Blue',
+    value: 'saga-blue',
+    icon: 'pi pi-apple',
+  },
+  {
+    backgroundColor: '#fff',
+    buttonColor: '#ff4081',
+    headingColor: '#3f51b5',
+    label: 'Vela Orange',
+    value: 'vela-orange',
+  },
+  {
+    backgroundColor: '#fff',
+    buttonColor: '#ff4081',
+    headingColor: '#3f51b5',
+    label: 'Arya Orange',
+    value: 'arya-orange',
+  },
+  {
+    backgroundColor: '#fff',
+    buttonColor: '#ff4081',
+    headingColor: '#3f51b5',
+    label: 'Fluent Light',
+    value: 'fluent-light',
+  },
+];
